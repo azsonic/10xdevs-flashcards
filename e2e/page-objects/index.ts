c8496bf2f7fe3b57edb3176e79a6e75917bd2a27
azsonic <arunas.zulonas@gmail.com>
2026-01-06 22:57:07 +0100

    fix(e2e): add .js extensions to ES module exports in page-objects

--- e2e/page-objects/index.ts
+++ e2e/page-objects/index.ts
@@ -3,9 +3,11 @@
  * Central export point for all page objects and components
  */
 
-export { BasePage } from "./base.page";
-export { NavbarComponent } from "./navbar.component";
-export { LoginPage } from "./login.page";
-export { RegisterPage } from "./register.page";
-export { DashboardPage } from "./dashboard.page";
-export { GeneratePage } from "./generate.page";
+export { BasePage } from "./base.page.js";
+export { NavbarComponent } from "./navbar.component.js";
+export { LoginPage } from "./login.page.js";
+export { RegisterPage } from "./register.page.js";
+export { DashboardPage } from "./dashboard.page.js";
+export { GeneratePage } from "./generate.page.js";
+export { LibraryPage } from "./library.page.js";
+export { CreateManualDialogComponent } from "./create-manual-dialog.component.js";
